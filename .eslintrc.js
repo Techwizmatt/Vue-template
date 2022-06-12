e7d5c110e4385f24da5c35dd796f2860ba02db73
Matthew Maggio <me@techwizmatt.info>
2022-06-11 18:57:49 -0700

    Init Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,16 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "new-cap": "off",
+    "no-new": "off",
+    "vue/html-closing-bracket-spacing": [
+      "error",
+      {
+        "startTag": "never",
+        "endTag": "never",
+        "selfClosingTag": "never"
+      }
+    ]
   }
 }
